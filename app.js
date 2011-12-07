2105707464de59d514f79bdda82f0cd035014075
mickael couzinet <mcouzinet@gmail.com>
2011-12-08 00:32:33 +0100

    Création des users

--- app.js
+++ app.js
@@ -1,32 +1,71 @@
-
-/**
- * Module dependencies.
+/*
+ * Application Node.js / Baby-mother-connect
+ * Author : @mcouzinet / mcouzinet@gmail.com
  */
 
-var express = require('express');
+/*
+ *  MongoDB
+ */
 
-var app = module.exports = express.createServer();
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var cube = new Schema({
+    id  	  : Number,
+    titre     : String,
+    contenu   : String
+});
+
+var message = new Schema({
+    texte  	  : String,
+    date      : Date
+});
+
+var user = new Schema({
+    nom    		: String,
+    prenom      : String,
+	idsocle     : Number,
+	mail        : String,
+	tel		    : Number,
+	twitter		: String,
+	facebook    : String,
+	Timeline    : [message],
+	Cubes		: [cube]
+});
+
+var Messages = mongoose.model('message', message);
+var Cubes = mongoose.model('cube', cube);
+var Users = mongoose.model('user', user);
+mongoose.connect('mongodb://92.243.19.190/baby');
+
+
+/**
+ *  Modules
+ */
+
+var express = require('express'),
+    _ = require('underscore'),
+    path = require('path'),
+    url = require('url'),
+	app = module.exports = express.createServer(),
+    io = require('socket.io').listen(app);
 
 // Configuration
-
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(app.router);
+  app.use(express.cookieParser());
   app.use(express.static(__dirname + '/public'));
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(app.router);
+  app.use(express.session({ secret: 'secretKey' }));
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
 
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
-});
-
-// Routes
+/**
+ *  Routes
+ */
 
 app.get('/', function(req, res){
   res.render('index', {
@@ -34,5 +73,32 @@ app.get('/', function(req, res){
   });
 });
 
+app.get('/addUser', function(req, res){
+  res.render('addUser',{
+	title: 'addUser'
+  });
+});
+
+app.post('/addUser', function(req, res){
+var user = new Users({ 
+    nom    		: req.param('nom'),
+    prenom      : req.param('prenom'),
+	idsocle     : req.param('idsocle'),
+	mail        : req.param('mail'),
+	tel		    : req.param('tel'),
+	twitter		: req.param('twitter'),
+	facebook    : req.param('facebook'),
+	Timeline    : new Array(),
+	Cubes		: new Array(),
+});
+user.save(function (err) { if (err) console.log('mongo: ', err); });
+  res.render('addUser',{
+	title: 'addUser'
+  });
+});
+
+/**
+ *  Start
+ */
+
 app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
